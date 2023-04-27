405fe7b5ba6449f8b497daed071d957f7f766975
OisinWrites <oisin.banville@gmail.com>
2023-04-27 10:47:48 +0000

    Create Byte project and apps

---
+++ Byte_Restaurant_Bookings/Byte_Restaurant_Bookings/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Byte_Restaurant_Bookings project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Byte_Restaurant_Bookings.settings')
+
+application = get_wsgi_application()
